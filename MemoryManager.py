b8b1072cbbd91367f1518890a8e1720126fa62be
CesarMtzV <mc.cesar.art@gmail.com>
2022-11-03 11:05:40 -0600

    Changed local temp arrangement

--- MemoryManager.py
+++ MemoryManager.py
@@ -45,7 +45,8 @@ class MemoryManager:
         """
         Retorna un entero que representa la memoria donde esta guardada la variable
         """
-    
+
+        # TODO: Checar si tambien cambiar temporales globales
         if scope == "global":
 
             # INT GLOBAL VARIABLES
@@ -124,15 +125,8 @@ class MemoryManager:
                     return assigned_address
                 else:
                     raise MemoryManagerException("Too many LOCAL INT variables")
-            elif type == "temp_int":
-                if self.local_temp_int < 11000:
-                    assigned_address = self.local_temp_int
-                    self.local_temp_int += size
-                    return assigned_address
-                else:
-                    raise MemoryManagerException("Too many LOCAL TEMP INT variables")
             
-            # FLOAT GLOBAL VARIABLES
+            # FLOAT LOCAL VARIABLES
             elif type == "float":
                 if self.local_float < 12000:
                     assigned_address = self.local_float
@@ -140,15 +134,8 @@ class MemoryManager:
                     return assigned_address
                 else:
                     raise MemoryManagerException("Too many LOCAL FLOAT variables")
-            elif type == "temp_float":
-                if self.local_temp_float < 13000:
-                    assigned_address = self.local_temp_float
-                    self.local_temp_float += size
-                    return assigned_address
-                else:
-                    raise MemoryManagerException("Too many LOCAL TEMP FLOAT variables")
             
-            # BOOL GLOBAL VARIABLES
+            # BOOL LOCAL VARIABLES
             elif type == "bool":
                 if self.local_bool < 14000:
                     assigned_address = self.local_bool
@@ -156,15 +143,8 @@ class MemoryManager:
                     return assigned_address
                 else:
                     raise MemoryManagerException("Too many LOCAL BOOL variables")
-            elif type == "temp_bool":
-                if self.local_temp_bool < 15000:
-                    assigned_address = self.local_temp_bool
-                    self.local_temp_bool += size
-                    return assigned_address
-                else:
-                    raise MemoryManagerException("Too many LOCAL TEMP BOOL variables")
             
-            # STRING GLOBAL VARIABLES
+            # STRING LOCAL VARIABLES
             elif type == "string":
                 if self.local_string < 16000:
                     assigned_address = self.local_string
@@ -172,16 +152,40 @@ class MemoryManager:
                     return assigned_address
                 else:
                     raise MemoryManagerException("Too many LOCAL STRING variables")
-            elif type == "temp_string":
+            
+            else:
+                raise MemoryManagerException(f"Undefined type: {type}")
+         
+        elif scope == "local_temp":
+            if type == "int":
+                if self.local_temp_int < 11000:
+                    assigned_address = self.local_temp_int
+                    self.local_temp_int += size
+                    return assigned_address
+                else:
+                    raise MemoryManagerException("Too many LOCAL TEMP INT variables")
+            elif type == "float":
+                if self.local_temp_float < 13000:
+                    assigned_address = self.local_temp_float
+                    self.local_temp_float += size
+                    return assigned_address
+                else:
+                    raise MemoryManagerException("Too many LOCAL TEMP FLOAT variables")
+            elif type == "bool":
+                if self.local_temp_bool < 15000:
+                    assigned_address = self.local_temp_bool
+                    self.local_temp_bool += size
+                    return assigned_address
+                else:
+                    raise MemoryManagerException("Too many LOCAL TEMP BOOL variables")
+            elif type == "string":
                 if self.local_temp_string < 17000:
                     assigned_address = self.local_temp_string
                     self.local_temp_string += size
                     return assigned_address
                 else:
                     raise MemoryManagerException("Too many LOCAL TEMP STRING variables")
-            else:
-                raise MemoryManagerException(f"Undefined type: {type}")
-         
+
         elif scope == "constant":
             # TODO: Revisar si tambien hay que agregar variables temporales para constantes
             if type == "int":
